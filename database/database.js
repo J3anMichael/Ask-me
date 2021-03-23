3add787de34c0ad701ecdb5506b388f5e42d599d
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-03-22 21:16:08 -0300

    line 3
    Random password!

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiPerguntas', 'root', '99051943Mae',{
+const connection = new Sequelize('guiPerguntas', 'root', '********',{
     host: 'localhost',
     dialect: 'mysql'
 });
